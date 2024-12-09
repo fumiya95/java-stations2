6d9cc0bcd4716177d9f0810f6af332b753d41ce0
fumiya95 <sakura_wanwan1111@docomo.ne.jp>
2024-12-09 21:16:28 +0900

    Update files with new changes

--- src/station1/Station1.java
+++ src/station1/Station1.java
@@ -9,11 +9,23 @@ public class Station1 {
 
     public static ArrayList<String> q1() {
         // 問題1: ここから
+        ArrayList<String> list = new ArrayList<>(); // 変数listを宣言
+        list.add("Tech");
+        list.add("Train");
+        return list;
         // 問題1: ここまで
+
     }
 
     public static int q2(ArrayList<String> list, ArrayList<String> searchWords) {
         // 問題2: ここから
+        int count = 0;
+        for (String word : searchWords) {
+            if (list.contains(word)) {
+                count++;
+            }
+        }
+        return count;
         // 問題2: ここまで
     }
 }
--- src/station3/Station3.java
+++ src/station3/Station3.java
@@ -7,11 +7,19 @@ public class Station3 {
 
     public static boolean checkEmailAddress(String email) {
         // 問題1: ここから
+        if (email != null) {
+            return !email.isEmpty() && email.contains("@");
+        } else {
+            return false;
+        }
         // 問題1: ここまで
     }
 
     public static void q2(User user) {
         // 問題2: ここから
+        if (user != null && user.name == null) {
+            user.name = "未設定";
+        }
         // 問題2: ここまで
     }
 }
